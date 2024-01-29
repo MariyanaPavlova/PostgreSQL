e2b0e5425c5a9d8e4177b34c8b2b72d29e725d66
Mariyana Pavlova <94930314+MariyanaPavlova@users.noreply.github.com>
2024-01-29 19:27:33 +0200

    add

--- 7.Database Programmability/Exercises_Database Programmability.sql
+++ 7.Database Programmability/Exercises_Database Programmability.sql
@@ -1,2 +1,22 @@
 --01
+CREATE or REPLACE FUNCTION fn_full_name(
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+) RETURNS VARCHAR(101)
+AS
+$$
+DECLARE 
+	full_name VARCHAR(101);
+BEGIN
+	full_name := INITCAP(first_name) || ' ' || INITCAP(last_name);
+	RETURN full_name;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+--02
+
+
+
 
--- 7.Database Programmability/Lab_Database Programmability.sql
+++ 7.Database Programmability/Lab_Database Programmability.sql
@@ -3,6 +3,9 @@
 CREATE OR REPLACE FUNCTION fn_full_name(VARCHAR, VARCHAR)
 RETURNS VARCHAR AS
 $$
+ 	DECLARE
+ 		first_name ALIAS FOR $1;
+ 		last_name ALIAS FOR $2;
  	BEGIN
 		RETURN concat($1, ' ', $2);
 	END
@@ -47,7 +50,8 @@ $$
 			USING(address_id)
 			JOIN towns as t
 			USING(town_id)
-		WHERE t.name = town_name INTO town_count;
+		WHERE t.name = town_name 
+		INTO town_count;
 		RETURN town_count;
 	END
 
@@ -68,6 +72,115 @@ LANGUAGE plpqsql;
 CALL sp_add_person(first_name: 'Milcho', last_name: 'Kolev', city_name: 'Burgas');
 SELECT * FROM persons
 
+--02--
+CREATE PROCEDURE sp_increase_salaries(department_name VARCHAR)
+AS 
+$$
+BEGIN
+	UPDATE employees
+	set salary = salary + salary * 0.05
+	WHERE department_id = 
+	(
+		SELECT
+		d.department_id
+		FROM employees as e
+		JOIN departments as d
+		USING (department_id)
+		WHERE name = department_name
+		GROUP BY d.department_id
+);
+END;
+$$
+LANGUAGE plpgsql;
+CALL sp_increase_salaries('Sales');
 
----
+---RAISE NOTICE-
+CREATE OR REPLACE FUNCTION fn_test_function(first_name VARCHAR)
+RETURNS INT AS 
+$$
+	BEGIN
+		RAISE NOTICE '%', first_name;
+		RETURN NULL;
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT fn_test_function('Koko')
+
+---Transacions--
+BEGIN;
+UPDATE accounts SET balance = balance - 100 WHERE account_id = 1;
+UPDATE accounts SET balance = balance + 100 WHERE account_id = 2;
+DECLARE
+	bob_balance DECIMAL(20,2);
+BEGIN
+	SELECT balance INTO bob_balance FROM accounts WHERE account_id = 2;
+	IF bob_balance > 1000 THEN
+		RAISE NOTICE 'Bob has too much money.Rolling back trn.';
+		ROLLBACK;
+		RETURN;
+	END IF;
+END;
+
+
+CREATE PROCEDURE sp_increase_salary_by_id(id int)
+AS 
+$$
+	BEGIN
+		IF(SELECT salary 
+		   FROM employees 
+		   WHERE employee_id = id) 
+		   IS NULL THEN RETURN;
+		ELSE
+		   UPDATE employees
+		   SET salary=salary + salary * 0.05
+		   WHERE employee_id = id;
+		END IF;
+		COMMIT;
+	END;
+$$
+LANGUAGE plpgsql;
+
+---Trigger--
+
+CREATE TABLE deleted_employees(
+	employee_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	middle_name VARCHAR(20),
+	job_title VARCHAR(50),
+	department_id INT,
+	salary NUMERIC(19,4)
+);
+
+CREATE OR REPLACE FUNCTION backup_fired_employees()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	INSERT INTO deleted_employees(
+		first_name,
+		last_name,
+		middle_name,
+		job_title,
+		department_id,
+		salary
+		)
+	VALUES(
+	old.first_name,
+	old.last_name,
+	old.middle_name,
+	old.job_title,
+	old.department_id,
+	old.salary
+	);
+	RETURN new;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER backup_employees
+AFTER DELETE on employees
+FOR EACH ROW
+EXECUTE PROCEDURE backup_fired_employees();
+
 
