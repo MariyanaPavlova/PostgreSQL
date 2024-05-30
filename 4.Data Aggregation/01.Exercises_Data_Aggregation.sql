dfeef9975b1ad6721c7dd047d325f17cb92e09da
Mariyana Pavlova <milkova.mariyana@gmail.com>
2024-05-30 16:04:41 +0300

    Update 01.Exercises_Data_Aggregation.sql

--- 4.Data Aggregation/01.Exercises_Data_Aggregation.sql
+++ 4.Data Aggregation/01.Exercises_Data_Aggregation.sql
@@ -121,17 +121,20 @@ SELECT
 FROM employees;
 
 --14.Update Employees' Data
-SELECT 
-	job_title,
-	CASE 
-		WHEN AVG(salary) > 45800 THEN 'Good'
-		WHEN AVG(salary) BETWEEN 27500 AND 45800 THEN 'Medium'
-		WHEN AVG(salary) < 27500 THEN 'Need Improvement'
-	END AS "category"
+UPDATE employees 
+	SET salary =
+		CASE
+			WHEN hire_date < '2015-01-16' THEN salary +2500
+			WHEN hire_date < '2020-03-04' THEN salary +1500
+			ELSE salary
+		END,
 	
-FROM employees
-GROUP BY job_title
-ORDER BY "category" ASC, job_title ASC
+	job_title = 
+		CASE
+			WHEN hire_date < '2015-01-16' THEN concat('Senior ', job_title)
+			WHEN hire_date < '2020-03-04' THEN concat('Mid-' , job_title)
+			ELSE job_title
+		END
 
 --15.Categorizes Salary
 SELECT 
