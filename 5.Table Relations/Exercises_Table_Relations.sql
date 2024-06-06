842713cfc6961eb333f6b149b063e188648e9a29
Mariyana Pavlova <milkova.mariyana@gmail.com>
2024-06-06 11:42:07 +0300

    Update Exercises_Table_Relations.sql

--- 5.Table Relations/Exercises_Table_Relations.sql
+++ 5.Table Relations/Exercises_Table_Relations.sql
@@ -310,7 +310,7 @@ CREATE TABLE customers(
 
 CREATE TABLE contacts(
 	id SERIAL PRIMARY KEY,
-	contact_name VARCHAR(50),
+	customer_name VARCHAR(50),
 	phone VARCHAR(50),
 	email VARCHAR(50),
 	customer_id INT,
