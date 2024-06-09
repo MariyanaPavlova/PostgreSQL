bfccea4e30103266b657efb761aa53a230717dd7
Mariyana Pavlova <milkova.mariyana@gmail.com>
2024-06-09 22:16:26 +0300

    Update Exercises_Database Programmability.sql

--- 7.Database Programmability/Exercises_Database Programmability.sql
+++ 7.Database Programmability/Exercises_Database Programmability.sql
@@ -84,32 +84,34 @@ where is_finished = true
 
 
 --05.Difficulty Level
-CREATE OR REPLACE FUNCTION fn_difficulty_level(level INT)
-RETURNS VARCHAR(50)
-
+CREATE or REPLACE FUNCTION fn_difficulty_level(
+	level varchar
+)
+RETURNS varchar
 AS
 $$
-DECLARE difficulty_level VARCHAR(50);
-BEGIN
-	IF (level <=40) THEN 
-		difficulty_level :='Normal Difficulty';
-	ELSEIF (level BETWEEN 41 AND 60) THEN 
-		difficulty_level:= 'Nightmare Difficulty';
-	ELSE
-		difficulty_level:= 'Hell Difficulty';
-	END IF;
-	RETURN difficulty_level;
-END;
+DECLARE deff_level varchar;
+
+	BEGIN
+		if level <= 40 THEN
+			deff_level :=  "Normal Difficulty";
+		elseif level BETWEEN 41 and 60 THEN
+			deff_level :=  "Nightmare Difficulty";
+		else
+			deff_level :=  "Hell Difficulty";
+		end if;
+		RETURN diff_level;
+	END;
 $$
 LANGUAGE plpgsql;
 
-SELECT 
+select 
 	user_id,
 	level,
-	cash, 
+	cash,
 	fn_difficulty_level(level)
-FROM users_games
-ORDER BY user_id ASC;
+from users_games
+order by user_id asc;
 
 
 --*06.Cash in User Games Odd Rows
