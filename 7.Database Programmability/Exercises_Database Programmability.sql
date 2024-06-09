6dc8d23be046c5e3efebd2d99b2c7de29974f164
Mariyana Pavlova <milkova.mariyana@gmail.com>
2024-06-09 21:28:46 +0300

    Update Exercises_Database Programmability.sql

--- 7.Database Programmability/Exercises_Database Programmability.sql
+++ 7.Database Programmability/Exercises_Database Programmability.sql
@@ -1,48 +1,51 @@
 --01.User-defined Function Full Name
-CREATE or REPLACE FUNCTION fn_full_name(
-	first_name VARCHAR(50),
-	last_name VARCHAR(50)
-) RETURNS VARCHAR(101)
+CREATE or REPLACE FUNCTION fn_full_name(first_name varchar, last_name varchar)
+RETURNS varchar 
 AS
 $$
-DECLARE 
-	full_name VARCHAR(101);
-BEGIN
-	full_name := INITCAP(first_name) || ' ' || INITCAP(last_name);
-	RETURN full_name;
-END;
+	DECLARE 
+	full_name varchar;
+	BEGIN
+		full_name := concat(INITCAP(first_name), ' ', INITCAP(last_name));
+		return full_name;
+	END;
 $$
 LANGUAGE plpgsql;
 
 
 --02.User-defined Function Future Value
-CREATE or REPLACE FUNCTION fn_calculate_future_value (
-	initial_sum DECIMAL,
-	yearly_interest_rate DECIMAL,
-	number_of_years INT
-) RETURNS DECIMAL AS 
+CREATE or REPLACE FUNCTION fn_calculate_future_value(
+	initial_sum decimal,
+	yearly_interest_rate decimal,
+	number_of_years int
+)
+RETURNS decimal 
+AS
 $$
-BEGIN
-	RETURN TRUNC(
-		initial_sum * POWER(1+yearly_interest_rate,	number_of_years),
-		4
-	);
-END;
+	DECLARE 
+	output decimal;
+	output1 decimal;
+	BEGIN
+		output := initial_sum * power(1 + yearly_interest_rate, number_of_years);
+		output1 := trunc(output, 4);
+	return output1;
+	END;
 $$
 LANGUAGE plpgsql;
 
 SELECT * FROM fn_calculate_future_value (1000, 0.1, 5)
 
 --03.User-defined Function Is Word Comprised
-CREATE OR REPLACE FUNCTION fn_is_word_comprised(
-	set_of_letters VARCHAR(50),
-	word VARCHAR(50)
-) RETURNS BOOLEAN
+CREATE or REPLACE FUNCTION fn_is_word_comprised(
+	set_of_letters varchar(50),
+	word varchar(50)
+)
+RETURNS boolean 
 AS
 $$
-BEGIN
-	RETURN TRIM(LOWER(word), LOWER(set_of_letters)) = '';
-END;
+	BEGIN
+		RETURN trim(lower(word), lower(set_of_letters)) = '';
+	END;
 $$
 LANGUAGE plpgsql;
 
