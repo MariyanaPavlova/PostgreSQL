8126684f2c2d3532468a644ce6caa48c302a9b42
Mariyana Pavlova <milkova.mariyana@gmail.com>
2024-06-15 08:28:54 +0300

    Add files via upload

---
+++ Exam prep/Exam III.sql
@@ -0,0 +1,198 @@
+board_games_db
+
+
+Section 1. 
+-----------
+create table categories(
+	id serial primary key,
+	name varchar(30) not null
+);
+
+create table addresses(
+	id serial primary key,
+	street_name varchar(100) not null,
+	street_number int not null CHECK(street_number > 0),
+	town varchar(30) not null, 
+	country varchar(50) not null,
+	zip_code int not null CHECK(zip_code > 0)
+	
+--	constraint street_number_check CHECK(street_number > 0),
+--	constraint zip_code_check CHECK(zip_code > 0)
+);
+
+create table publishers(
+	id serial primary key,
+	name varchar(30) not null,
+	address_id int REFERENCES addresses on delete cascade on update cascade not null,
+	website varchar(40),
+	phone varchar(20)
+);
+create table players_ranges(
+	id serial primary key,
+	min_players int not null,
+	max_players int not null,
+
+	constraint min_players_check CHECK(min_players > 0),
+	constraint max_players_check CHECK(max_players > 0)
+);
+
+create table creators(
+	id serial primary key,
+	first_name varchar(30) not null,
+	last_name varchar(30) not null,
+	email varchar(30) not null
+);
+
+create table board_games(
+	id serial primary key,
+	name varchar(30) not null,
+	release_year int not null,
+	rating NUMERIC(4, 2) not null,
+	category_id int REFERENCES categories on delete cascade on update cascade not null,
+	publisher_id int REFERENCES publishers on delete cascade on update cascade not null,
+	players_range_id int REFERENCES players_ranges on delete cascade on update cascade not null
+	
+	constraint release_year_check CHECK(release_year > 0)
+);
+
+
+create table creators_board_games(
+	creator_id int REFERENCES creators on delete cascade on update cascade not null,
+	board_game_id int REFERENCES board_games on delete cascade on update cascade not null
+	
+);
+
+
+Section 2.
+-----------
+3.5--
+SELECT 
+	name, 
+	rating
+FROM board_games
+ORDER BY 
+    release_year ASC, name DESC;
+
+3.6--
+SELECT 
+    bg.id,
+    bg.name,
+    bg.release_year,
+    c.name
+FROM 
+    board_games as bg
+JOIN 
+    categories as c ON c.id = bg.category_id
+
+WHERE c.name IN ('Strategy Games', 'Wargames')
+ORDER BY 
+    bg.release_year DESC;
+    
+3.7--
+SELECT 
+    c.id,
+    CONCAT(c.first_name, ' ', c.last_name) AS creator_name,
+    c.email
+
+FROM 
+    creators as c
+left join creators_board_games as cbg
+	on cbg.creator_id = c.id
+
+WHERE 
+    cbg.creator_id IS NULL
+ORDER BY 
+    creator_name ASC;
+    
+
+3.8--0 точки
+SELECT 
+    bg.name,
+    bg.rating,
+    c.name as category_name
+
+FROM 
+    board_games as bg
+left JOIN 
+    categories as c ON c.id = bg.category_id
+left join 
+	players_ranges as pr on pr.id = bg.players_range_id
+	
+	WHERE 
+    (bg.name LIKE '%a%' and bg.rating > 7.00)
+	OR (bg.rating > 7.50
+       AND pr.min_players >= 2
+		and pr.max_players <= 5)
+	
+ORDER BY 
+    bg.name ASC,
+    bg.rating DESC
+LIMIT 5;
+  
+3.9--
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+     c.email,
+	MAX(bg.rating) as rating
+FROM creators as c
+JOIN creators_board_games as cbg
+on cbg.creator_id = c.id
+JOIN board_games as bg 
+ON cbg.board_game_id = bg.id
+
+	WHERE c.email LIKE '%.com'
+	group by c.id, full_name, c.email
+order by full_name
+
+3.10--
+SELECT
+    c.last_name,
+	ceiling((bg.rating)) as avarage_rating,
+	p.name as publisher_name
+FROM creators as c
+JOIN creators_board_games as cbg
+	on cbg.creator_id = c.id
+JOIN board_games as bg 
+	ON cbg.board_game_id = bg.id
+join publishers as p
+	on bg.publisher_id = p.id
+where p.name = 'Stonemaier Games'
+order by avarage_rating desc, last_name
+ 
+Section 3.
+----------
+	
+    SELECT COUNT(bg.id)
+    FROM board_games as bg
+	join creators_board_games as cbg
+		on cbg.board_game_id = bg.id
+		
+    JOIN creators as c ON c.id = cbg.creator_id
+    WHERE c.first_name = 'Bruno';
+
+
+CREATE OR REPLACE FUNCTION fn_creator_with_board_games(creator_first_name VARCHAR(30))
+RETURNS INTEGER 
+AS 
+$$
+DECLARE
+    total_board_games INTEGER;
+BEGIN
+    SELECT COUNT(bg.id) INTO total_board_games
+    FROM board_games as bg
+	join creators_board_games as cbg
+		on cbg.board_game_id = bg.id
+		
+    JOIN creators as c ON c.id = cbg.creator_id
+    WHERE c.first_name = creator_first_name;
+
+    RETURN total_board_games;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT fn_creator_with_board_games('Bruno')
+SELECT fn_creator_with_board_games('Alexander')
+
+Section 4.
+----------
+
